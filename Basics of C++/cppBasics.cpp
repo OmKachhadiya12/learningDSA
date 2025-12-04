03b24541ddc488f229c7b68528e2e7d795e0db33
om kachhadiya <kachhadiyaom2005@gmail.com>
2025-12-04 16:05:30 +0530

    Learning the CPP basics.

---
+++ Basics of C++/cppBasics.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    //1.Input/Output 
+    // int number;
+    // cout << "Enter your lucky number:";
+    // cin >> number;
+    // cout << "Hey there!! your number is:" << number;
+
+    //2.Condition statements 
+    //int age;
+    // cout << "Enter your age to check out you are an Adult or out?";
+    // cin >> age;
+
+    // if(age >= 18) {
+    //     cout << "You are an adult.";
+    // }
+    // else {
+    //     cout << "You aren't an adult.";
+    // }
+
+    //3.Switch Statement 
+    // int day;
+    // cout << "Enter any number between 1 to 7: ";
+    // cin >> day;
+
+    // switch (day)
+    // {
+    // case 1: 
+    //     cout << "Monday";
+    //     break;
+    // case 2: 
+    //     cout << "Tuesday";
+    //     break;
+    // case 3: 
+    //     cout << "Wednesday";
+    //     break;
+    // case 4: 
+    //     cout << "Thrusday";
+    //     break;
+    // case 5: 
+    //     cout << "Friday";
+    //     break;
+    // case 6: 
+    //     cout << "Saturaday";
+    //     break;
+    // case 7: 
+    //     cout << "Sunday";
+    //     break;
+    
+    // default:
+    //     cout << "Not a Valid number!!";
+    //     break;
+    // }
+
+    return 0;
+}
