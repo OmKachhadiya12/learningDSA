026ff7cbd216a64004a30b2f350f6fd9fbd90b89
om kachhadiya <kachhadiyaom2005@gmail.com>
2025-12-05 17:58:47 +0530

    Removing the comments from the codes.

--- Basics of C++/cppBasics.cpp
+++ Basics of C++/cppBasics.cpp
@@ -2,105 +2,76 @@
 using namespace std;
 
 //9.Function
-// int addition(int x, int y) {
-//     int sum = x + y;
-//     return sum;
-// }
+int addition(int x, int y) {
+    int sum = x + y;
+    return sum;
+}
 
 int main() {
 
     //1.Input/Output 
-    // int number;
-    // cout << "Enter your lucky number:";
-    // cin >> number;
-    // cout << "Hey there!! your number is:" << number;
+    int number;
+    cout << "Enter your lucky number:";
+    cin >> number;
+    cout << "Hey there!! your number is:" << number;
 
     //2.Condition statements 
-    //int age;
-    // cout << "Enter your age to check out you are an Adult or out?";
-    // cin >> age;
+    int age;
+    cout << "Enter your age to check out you are an Adult or out?";
+    cin >> age;
 
-    // if(age >= 18) {
-    //     cout << "You are an adult.";
-    // }
-    // else {
-    //     cout << "You aren't an adult.";
-    // }
+    if(age >= 18) {
+        cout << "You are an adult.";
+    }
+    else {
+        cout << "You aren't an adult.";
+    }
 
     //3.Switch Statement 
-    // int day;
-    // cout << "Enter any number between 1 to 7: ";
-    // cin >> day;
-
-    // switch (day)
-    // {
-    // case 1: 
-    //     cout << "Monday";
-    //     break;
-    // case 2: 
-    //     cout << "Tuesday";
-    //     break;
-    // case 3: 
-    //     cout << "Wednesday";
-    //     break;
-    // case 4: 
-    //     cout << "Thrusday";
-    //     break;
-    // case 5: 
-    //     cout << "Friday";
-    //     break;
-    // case 6: 
-    //     cout << "Saturaday";
-    //     break;
-    // case 7: 
-    //     cout << "Sunday";
-    //     break;
-    
-    // default:
-    //     cout << "Not a Valid number!!";
-    //     break;
-    // }
+    int day;
+    cout << "Enter any number between 1 to 7: ";
+    cin >> day;
 
     //4.Array
-    //int arr[5];
-    // cout << "Enter the elements in the array:";
+    int arr[5];
+    cout << "Enter the elements in the array:";
 
-    // cin >> arr[0] >>  arr[1] >> arr[2] >> arr[3] >> arr[4] >> arr[4];
+    cin >> arr[0] >>  arr[1] >> arr[2] >> arr[3] >> arr[4] >> arr[4];
 
-    // cout << arr[2];
+    cout << arr[2];
 
     //5.String 
-    // string s = "Kachhadiya";
-    // int len  = s.size();
+    string s = "Kachhadiya";
+    int len  = s.size();
 
-    // cout << s[len-1];
+    cout << s[len-1];
 
     //6.For loop
-    // for(int i=1; i<=10; i++) {
-    //     cout << "om" << "\n";
-    // }
+    for(int i=1; i<=10; i++) {
+        cout << "om" << "\n";
+    }
 
     //7.While loop
-    // int i = 1; 
-    // while (i<=10){
+    int i = 1; 
+    while (i<=10){
 
-    //     cout << "om" << endl;
-    //     i++; 
-    // }
+        cout << "om" << endl;
+        i++; 
+    }
 
     //8.Do-While loop
-    // int i = 1;
-    // do{
-    //     cout << "om" << endl;
-    //     i++;
-    // }while(i<=5);
+    int i = 1;
+    do{
+        cout << "om" << endl;
+        i++;
+    }while(i<=5);
 
     //9.Function
-    // int a,b;
-    // cout << "Enter any two number to do addition: ";
-    // cin >> a >> b;
-    // int sum = addition(a,b);
-    // cout << "The addition is " << sum;
+    int a,b;
+    cout << "Enter any two number to do addition: ";
+    cin >> a >> b;
+    int sum = addition(a,b);
+    cout << "The addition is " << sum;
 
     return 0;
 }
