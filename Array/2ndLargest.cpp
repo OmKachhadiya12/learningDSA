c17437190a56ef29a264779711e06a55294b338d
om kachhadiya <kachhadiyaom2005@gmail.com>
2025-12-21 15:34:18 +0530

    Add the Second largest element of an Array.

--- Array/2ndLargest.cpp
+++ Array/2ndLargest.cpp
@@ -3,8 +3,60 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int largest(int arr[], int n) {
+int slargest(int arr[], int n) {
+    // int maxi = arr[0];
+    // int deletee;
+    // for (int i = 1; i < n; i++) {
+    //     if (arr[i] > maxi) {
+    //         maxi = arr[i];
+    //         deletee = i;
+    //     }
+    // }
+    // arr[deletee] = 0;
+
+    // int maxii = arr[0];
+
+    // for (int i = 1; i < n; i++) {
+    //     if (arr[i] > maxii) {
+    //         maxii = arr[i];
+    //     }
+    // }
+
+    // return maxii;
+
+    // int maxii = arr[0];
+
+    // for (int i = 1; i < n; i++) {
+    //     if (arr[i] > maxii) {
+    //         maxii = arr[i];
+    //     }
+    // }
+
+    // int smaxii = -1;
+
+    // for (int i = 0; i < n; i++) {
+    //     if (arr[i] > smaxii && arr[i] != maxii) {
+    //         smaxii = arr[i];
+    //     }
+    // }
+
+    // return smaxii;
+
+    int maxii = arr[0];
+    int smaxii = INT_MIN;
+
+    for (int i=0;i<n;i++){
+        if(arr[i] > maxii) {
+            smaxii = maxii;
+            maxii = arr[i];
+        } else if(arr[i] > smaxii && arr[i] != maxii) {
+            smaxii = arr[i];
+        }
+    }
+
+    return smaxii;
     
+
 }
 
 int main() {
@@ -16,6 +68,9 @@ int main() {
         cin >> arr[i];
     }
 
-    cout << largest(arr, n);
+    int maxi = slargest(arr, n);
+
+    cout << endl << maxi; 
+    
     return 0;
 }
