d7d66b2d21d16d22813a214a7f5d62f222f7b6bf
om kachhadiya <kachhadiyaom2005@gmail.com>
2025-12-21 11:58:00 +0530

    Add the largest emlemnt of an Array.

---
+++ Array/largestElement.cpp
@@ -0,0 +1,28 @@
+//Largest element
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int largest(int arr[], int n) {
+    int maxi = arr[0];
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > maxi) {
+            maxi = arr[i];
+        }
+    }
+    return maxi;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    cout << largest(arr, n);
+    return 0;
+}
