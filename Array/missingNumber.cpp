1c39000455a1efe3cd3ec71369d09709f31992f2
om kachhadiya <kachhadiyaom2005@gmail.com>
2025-12-26 21:59:59 +0530

    Add the Missing Number.

---
+++ Array/missingNumber.cpp
@@ -0,0 +1,49 @@
+//Missing Number
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int missingNumber(int arr[],int n) {
+    // int hash[n+1] = {0};
+
+    // for(int i=0;i<n;i++) {
+    //     hash[arr[i]] = 1;
+    // }
+
+    // for (int i=0;i<=n;i++){
+    //     if(hash[i] == 0) {
+    //         return i;
+    //     }
+    // }
+    // return -1;
+
+    int sum1 = (n * (n + 1)) / 2;
+    int sum2 = 0;
+
+    for (int i=0;i<n;i++){
+        sum2+=arr[i];
+    }
+
+    return (sum1 - sum2);
+    
+
+}
+
+int main() {
+
+    int n;
+    cout << "Enter the Number: ";
+    cin >> n;
+
+    int arr[n];
+    for (int i=0;i<n;i++){
+        cin >> arr[i];
+    }
+
+    int missing = missingNumber(arr,n);
+
+    cout << missing;
+    
+
+    return 0;
+}
