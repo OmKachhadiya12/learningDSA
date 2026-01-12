02fcc1d21270835711b628606522df2cd54b1cfb
om kachhadiya <kachhadiyaom2005@gmail.com>
2026-01-12 18:33:22 +0530

    Add the optimal solution of majority element (n / 3).

--- Array/majorityElementn3.cpp
+++ Array/majorityElementn3.cpp
@@ -34,7 +34,90 @@ vector<int> majorElement(int arr[],int n) {
     
     // return temp;
 
-    
+    int el1,el2;
+    int count1 = 0,count2 = 0;
+
+    for (int i=0;i<n;i++){
+        if(count1 == 0 && arr[i] != el2) {
+            count1 = 1;
+            el1 = arr[i];
+        } else if(count2 == 0 && arr[i] != el1) {
+            count2 = 1;
+            el2 = arr[i];
+        } else if(arr[i] == el1) {
+            count1++;
+        } else if(arr[i] == el2) {
+            count2++;
+        } else {
+            count1--;
+            count2--;
+        }
+    }
+
+    count1 = 0;
+    count2 = 0; 
+    for (int i=0;i<n;i++){
+        if(arr[i] == el1) {
+            count1++;
+        } 
+        if(arr[i] == el2) {
+            count2++;
+        }
+    }
+    int need = (n / 3) + 1;
+    vector<int> temp;
+    if(count1 > need) {
+        temp.push_back(el1);
+    } 
+    if(count2 > need) {
+        temp.push_back(el2);
+    }
+
+    return temp;
+
+
+    // Mine leetcode solution Accepted
+
+    //     class Solution {
+    // public:
+    //     vector<int> majorityElement(vector<int>& arr) {
+    //         int el1 = 0, el2 = 0;
+    //         int count1 = 0, count2 = 0;
+
+    //         // Phase 1: Find candidates
+    //         for (int x : arr) {
+    //             if (x == el1) count1++;
+    //             else if (x == el2) count2++;
+    //             else if (count1 == 0) {
+    //                 el1 = x;
+    //                 count1 = 1;
+    //             }
+    //             else if (count2 == 0) {
+    //                 el2 = x;
+    //                 count2 = 1;
+    //             }
+    //             else {
+    //                 count1--;
+    //                 count2--;
+    //             }
+    //         }
+
+    //         // Phase 2: Verify
+    //         count1 = 0;
+    //         count2 = 0;
+    //         for (int x : arr) {
+    //             if (x == el1) count1++;
+    //             else if (x == el2) count2++;
+    //         }
+
+    //         vector<int> ans;
+    //         int n = arr.size();
+    //         if (count1 > n / 3) ans.push_back(el1);
+    //         if (count2 > n / 3) ans.push_back(el2);
+
+    //         return ans;
+    //     }
+    // };
 
 }
 
