4db4db3b1cda878b6c1d702b1b21c6713e57726f
om kachhadiya <kachhadiyaom2005@gmail.com>
2026-01-23 11:06:21 +0530

    Add the optimal solution for the Next Permutation problem.

--- Array/nextPermutation.cpp
+++ Array/nextPermutation.cpp
@@ -4,8 +4,36 @@
 using namespace std;
 
 vector<int> nextPair(vector<int> arr,int n) {
-    next_permutation(arr.begin(),arr.end());
-    return arr;
+    // next_permutation(arr.begin(),arr.end());
+    // return arr;
+
+    int n = arr.size();
+        int index = -1;
+
+        // Step 1: Find the break point
+        for (int i = n - 2; i >= 0; i--) {
+            if (arr[i] < arr[i + 1]) {
+                index = i;
+                break;
+            }
+        }
+
+        // If no break point found
+        if (index == -1) {
+            reverse(arr.begin(), arr.end());
+            return;
+        }
+
+        // Step 2: Find element just greater than arr[index]
+        for (int i = n - 1; i > index; i--) {
+            if (arr[i] > arr[index]) {
+                swap(arr[i], arr[index]);
+                break;
+            }
+        }
+
+        // Step 3: Reverse the right part
+        reverse(arr.begin() + index + 1, arr.end());
 }
 
 int main() {
