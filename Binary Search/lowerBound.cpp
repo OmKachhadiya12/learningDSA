c921f310c697a454a0bff7cde3ec0b9108081361
om kachhadiya <kachhadiyaom2005@gmail.com>
2026-01-23 17:28:47 +0530

    Add the Lower and Upper bound problem.

---
+++ Binary Search/lowerBound.cpp
@@ -0,0 +1,47 @@
+//Lower Bound
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int lowerBound(int arr[],int n,int target) {
+    int ans = n;
+    int low = 0;
+    int high = n - 1;
+
+    while (low <= high){
+        int mid = (low + high) / 2;
+        if(arr[mid] >= target) {
+            ans = mid;
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        } 
+    }
+    
+    return ans;
+
+} 
+
+int main() {
+
+    int n;
+    cout << "Enter  the size of an Array: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter the Elements: ";
+
+    for (int i=0;i<n;i++){
+        cin >> arr[i];
+    }
+
+    int target;
+    cout << "Enter the Target: ";
+    cin >> target;
+
+    int ans = lowerBound(arr,n,target);
+
+    cout << ans;
+
+    return 0;
+}
