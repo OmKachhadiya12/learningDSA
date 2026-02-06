9139986f60ddf821a4396a661e3e2315d8d2f4fd
om kachhadiya <kachhadiyaom2005@gmail.com>
2026-02-06 16:15:23 +0530

    Add the Search in 2D Matrix 2.

---
+++ Binary Search/searchIn2dMatrix2.cpp
@@ -0,0 +1,47 @@
+//Search in 2-D Marix.
+
+#include<bits/stdc++.h>
+using namespace std;
+
+bool search(vector<vector<int>> &arr,int x,int y,int target) {
+    int row = 0;
+    int col = y - 1;
+
+    while(row < x && col >= 0) {
+        if (arr[row][col] == target){
+            return true;
+        } else if(target < arr[row][col]) {
+            col--;
+        } else {
+            row++;
+        }
+    }
+
+    return false;
+}
+
+int main() {
+
+    int x,y;
+    cout << "Enter the Rows and Columns: ";
+    cin >> x >> y;
+
+    vector<vector<int>> arr(x,vector<int>(y));
+
+    for (int i=0;i<x;i++){
+        for (int j=0;j<y;j++){
+            cin >> arr[i][j];
+        }
+        cout << endl;
+    }
+
+    int target;
+    cout << "Enter the Target: ";
+    cin >> target;
+
+    bool ans = search(arr,x,y,target);
+
+    cout << ans;
+ 
+    return 0;
+}
