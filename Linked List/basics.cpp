cc6ed1f32e8a8b97109d0b5fe8573b5cb576b904
om kachhadiya <kachhadiyaom2005@gmail.com>
2026-02-18 11:19:56 +0530

    Add the Traveral , Searching and Length in Linked List.

--- Linked List/basics.cpp
+++ Linked List/basics.cpp
@@ -35,6 +35,65 @@ int lengthOfLL(Node* head) {
     
 }
 
+// Deletion of Head
+Node* deleteHead(Node* head) {
+    Node* temp = head;
+    head = head->next;
+    delete temp;
+    return head; 
+}
+
+// Deletion of Tail
+Node* deleteTail(Node* head) {
+    if(head == nullptr || head->next == nullptr) {
+        return nullptr;
+    }
+    
+    Node* curr = head;
+    while (curr->next->next != nullptr){
+        curr = curr->next;
+    }
+
+    Node* temp = curr->next;
+    curr->next = nullptr;
+    delete temp; 
+    
+    return head;
+}
+
+// Deletion K-th Element
+Node* deleteKthElement(Node* head,int k) {
+    if(head == nullptr) {
+        return head;
+    }
+    if(k == 1) {
+        Node* temp = head;
+        head = temp->next;
+        delete temp;
+        return head;
+    }
+
+    int count = 1;
+    Node* temp = head;
+    Node* prev = nullptr;
+
+    while (temp != nullptr){
+        prev = temp;
+        temp = temp->next; 
+        count++;
+        if(count == k) {
+            break;
+        }
+    }
+    if(temp == nullptr) {
+        return head;
+    }
+    prev->next = temp->next;
+    delete temp;
+    return head;
+    
+}
+
 int main() {
 
     // Node* n1 = new Node(2,nullptr);
@@ -59,11 +118,19 @@ int main() {
     Node* temp = head;
 
     while (temp != nullptr){
-        cout << temp->data;
+        cout << temp->data << "o";
         temp = temp->next;
     }
     
     int length = lengthOfLL(head);
+
+    head = deleteKthElement(head,3);
+    Node* tempp = head;
+
+    while (tempp != nullptr){
+        cout << tempp->data << "o";
+        tempp = tempp->next;
+    }
 
     return 0;
 }
