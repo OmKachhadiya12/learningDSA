7bc389df04b83c244fb209ae5f67435a32dccf0c
om kachhadiya <kachhadiyaom2005@gmail.com>
2026-02-19 18:41:17 +0530

    Add the Deletion of Doubly Linked list.

--- Linked List/doublyLinkedList.cpp
+++ Linked List/doublyLinkedList.cpp
@@ -34,6 +34,84 @@ Node* convertToDoubly(int arr[],int n) {
     return head;
 }
 
+// Deletion of Head
+Node* deleteHead(Node* head) {
+    if (head == nullptr || head->next == nullptr){
+        return nullptr;
+    }
+    
+    Node* temp = head;
+    head = head->next;
+    head->prev = nullptr;
+    temp->next = nullptr;
+    delete temp;
+    return head;
+}
+
+// Deletion of Tail
+Node* deleteTail(Node* head) {
+    if(head == nullptr || head->next == nullptr) {
+        return nullptr;
+    }
+
+    Node* curr = head;
+    while (curr->next->next != nullptr){
+        curr = curr->next;
+    }
+    
+    Node* temp = curr->next;
+    curr->next = nullptr;
+    temp->prev = nullptr;
+    delete temp;
+    return head;
+}
+
+// Deletion of K-th Pos
+Node* deleteKth(Node* head,int k) {
+    if(head == nullptr) {
+        return nullptr;
+    }
+
+    if(k == 1) {
+        Node* temp = head;
+        head = head->next;
+        if (head != nullptr){
+            head->prev = nullptr;
+        }
+        
+        temp->next = nullptr;
+        delete temp;
+        return head;
+    }
+
+    int count = 1;
+    Node* curr = head;
+
+    while (curr != nullptr && count < k){
+        curr = curr->next;
+        count++;
+    }
+
+    if(curr == nullptr) {
+        return head;
+    }
+    
+    Node* front = curr->next;
+    Node* back = curr->prev;
+
+    if (back != nullptr) {
+        back->next = front;
+    }
+
+    if (front != nullptr) {   
+        front->prev = back;
+    }
+    
+    delete curr;
+    return head;
+    
+}
+
 int main() {
 
     int arr[5] = {1,2,3,4,5};
@@ -46,6 +124,13 @@ int main() {
         temp = temp->next;
     }
     
+    cout << endl;
+    head = deleteKth(head,4);
+    Node* teemp = head;
+    while (teemp != nullptr){
+        cout << teemp->data;
+        teemp = teemp->next;
+    }
 
     return 0;
 }
