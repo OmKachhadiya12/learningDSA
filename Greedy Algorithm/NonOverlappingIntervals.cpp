02879c5853028b923b6e61b3cd9d80f66beb3c2c
om kachhadiya <kachhadiyaom2005@gmail.com>
2026-08-26 11:02:04 +0530

    Add the Non-Overlapping Intervals.

---
+++ Greedy Algorithm/NonOverlappingIntervals.cpp
@@ -0,0 +1,29 @@
+// Non-Overlapping Intervals.
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+
+    sort(intervals.begin(),intervals.end(),[](auto& a,auto& b) {
+        return a[1] < b[1];
+    });
+
+    int count = 0;
+    int lastEnd = intervals[0][1];
+
+    for (int i=1;i<intervals.size(); i++) {
+        if(intervals[i][0] < lastEnd) {
+            count++; 
+        } else{
+            lastEnd = intervals[i][0];
+        }
+    }
+    
+    return count;
+
+}
+
+int main() {
+    return 0;
+}
